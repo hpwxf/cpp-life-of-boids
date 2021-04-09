1cf99ecda848969f45ec510a798ab7da6b9158e1
Pascal HAVÉ <hpwxf@haveneer.com>
2021-04-09 22:07:28 +0200

    Add workaround for sudo command

--- .ci/install.sh
+++ .ci/install.sh
@@ -14,9 +14,24 @@ if [[ "$ENABLE_COVERAGE" == "on" ]] && [[ "$ENABLE_MEMCHECK" == "on" ]]; then
   exit 1
 fi
 
+ARCH=$(uname -s)
+case $ARCH in
+  Linux|Darwin)
+    SUDO=sudo
+    ;;
+  MSYS_NT*|MINGW64_NT*) # Windows
+    SUDO=
+    ;;
+  *)
+    echo "Unknown OS [$ARCH]"
+    exit 1
+    ;;
+esac
+
+
 if (! command -v conan >/dev/null 2>&1 ); then
   # install conan if not already installed
-  sudo pip3 install wheel setuptools
-  sudo pip3 install conan
-  # sudo ln -s ~/.local/bin/conan /usr/local/bin/conan
+  ${SUDO} pip3 install wheel setuptools
+  ${SUDO} pip3 install conan
+  # ${SUDO} ln -s ~/.local/bin/conan /usr/local/bin/conan
 fi
